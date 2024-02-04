12424702e57aad7cad232b7bed16ab42de8928fc
Asilbek2002 <school2924@gmail.com>
2024-02-05 02:28:23 +0500

    learn new lesson

---
+++ #23 Dynamic types/index.js
@@ -0,0 +1,29 @@
+// To string
+// 1-usul
+// console.log(typeof String(4));
+
+// 2-usul
+// console.log(typeof ("hello" + 3));
+
+// 3-usul
+// let num = 10;
+// console.log(typeof ("https://youtube/asilbek/" + num));
+
+// To number
+// 1 - usul
+// console.log(typeof Number("12"));
+
+//2-usul
+// console.log(typeof parseInt("hello"));
+
+//3-usul
+// console.log(typeof +"hel");
+
+// To boolean
+
+//0 , '' , NaN, undefined, null- xar doim false qaytaradi
+//1-usul
+// console.log(typeof Boolean("33"));
+
+//2-usul
+// console.log(!!"44");
