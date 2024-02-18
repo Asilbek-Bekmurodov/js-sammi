908b92a9809a52f028828e885ef8973a67de3072
Asilbek2002 <school2924@gmail.com>
2024-02-18 22:46:47 +0500

    amaliyot

--- #26 project/js/script.js
+++ #26 project/js/script.js
@@ -1,47 +1,98 @@
 "use strict";
 
-const adv = document.querySelectorAll("img");
-const promoBg = document.querySelector(".promo__bg");
-const promoGenre = document.querySelector(".promo__genre");
-const seriesList = document.querySelector(".promo__interactive-list");
-adv.forEach((item) => {
-  item.remove();
-});
+document.addEventListener("DOMContentLoaded", () => {
+  const adv = document.querySelectorAll("img");
+  const promoBg = document.querySelector(".promo__bg");
+  const promoGenre = document.querySelector(".promo__genre");
+  const seriesList = document.querySelector(".promo__interactive-list");
+  const form = document.querySelector(".add");
+  const inputVal = document.querySelector(".adding__input");
+  const checkBox = document.querySelector('[type="checkbox"]');
+  const btn = document.querySelector("button");
 
-const seriesDB = {
-  series: [
-    "Omar",
-    "The Final Legacy",
-    "hello",
-    "Magnificent Century",
-    "The Great Seljuks: Guardians...",
-  ],
-};
+  const seriesDB = {
+    series: [
+      "Omar",
+      "The Final Legacy",
+      "hello",
+      "Magnificent Century",
+      "The Great Seljuks: Guardians...",
+    ],
+  };
 
-promoGenre.innerText = "Comedy";
+  form.addEventListener("submit", (event) => {
+    event.preventDefault();
+    let newSeries = inputVal.value;
+    const favorite = checkBox.checked;
 
-// promoBg.style.backgroundImage = 'url("img/1.jpg")';
-promoBg.style.cssText = 'background-image: url("img/1.jpg")';
+    if (favorite === true) {
+      console.log("sevimli serial qoshildi");
+    }
 
-// seriesList.innerHTML = "";
+    if (newSeries.length > 18) {
+      // newSeries = `${newSeries.substring(0, 18)}...`;
+      newSeries = `${newSeries.substring(0, 18)}...`;
+    }
 
-// seriesDB.series.forEach((item, idx) => {
-//   seriesList.insertAdjacentHTML(
-//     "beforeend",
-//     `<li class="promo__interactive-item">
-//   ${item}
-//   <div class="delete"></div>
-//   </li>`
-//   );
-// });
+    if (newSeries) {
+      seriesDB.series.push(newSeries);
+      createList(seriesList, seriesDB.series);
+      event.target.reset();
+    }
+  });
 
-seriesList.innerHTML = "";
+  function changes() {
+    promoGenre.innerText = "Comedy";
+    // promoBg.style.backgroundImage = 'url("img/1.jpg")';
+    promoBg.style.cssText = 'background-image: url("img/1.jpg")';
+  }
 
-seriesDB.series.forEach((item, idx) => {
-  seriesList.innerHTML += `
-  <li class="promo__interactive-item">
-                ${idx + 1 + " " + item}
-                <div class="delete"></div>
-              </li>
-  `;
+  function advRemover(items) {
+    items.forEach((item) => {
+      item.remove();
+    });
+  }
+
+  function createList(parent, items) {
+    parent.innerHTML = "";
+    sortList(items);
+
+    items.forEach((item, idx) => {
+      parent.innerHTML += `
+    <li class="promo__interactive-item">
+      ${idx + 1 + " " + item}
+      <div class="delete"></div>
+    </li>
+    `;
+    });
+
+    // seriesList.innerHTML = "";
+
+    // seriesDB.series.forEach((item, idx) => {
+    //   seriesList.insertAdjacentHTML(
+    //     "beforeend",
+    //     `<li class="promo__interactive-item">
+    //   ${item}
+    //   <div class="delete"></div>
+    //   </li>`
+    //   );
+    // });
+
+    document.querySelectorAll(".delete").forEach((trash, idx) => {
+      trash.addEventListener("click", () => {
+        trash.parentElement.remove();
+        seriesDB.series.splice(idx, 1);
+        createList(parent, items);
+      });
+    });
+  }
+
+  function sortList(arr) {
+    arr.sort();
+  }
+
+  advRemover(adv);
+  createList(seriesList, seriesDB.series);
+  sortList(seriesDB.series);
+  changes();
 });
