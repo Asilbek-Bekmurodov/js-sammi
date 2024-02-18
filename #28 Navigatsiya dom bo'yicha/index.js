afbb25ca8f42e831eb2eaa99910fa703c9028aa4
Asilbek2002 <school2924@gmail.com>
2024-02-18 21:15:10 +0500

    #28 lesson upload

---
+++ #28 Navigatsiya dom bo'yicha/index.js
@@ -0,0 +1,32 @@
+// const head = document.head;
+// headni domdan olish
+
+// console.log(document.body.childNodes);
+// body bolalarini ko'rish ucun
+
+// console.log(document.body.lastChild);
+// console.log(document.body.firstChild.nextSibling);
+// birinchi va oxirgi element olib beradi
+
+// const btn = document.querySelector(".btn");
+// console.log(btn);
+
+// console.log(btn.parentNode);
+// element otasini tanlash uchun
+
+// console.log(document.querySelector('[data-current="b"]'));
+// data atributi orqali element olish
+
+// console.log(btn.parentNode.nextSibling.nextSibling);
+// console.log(btn.parentNode.nextElementSibling);
+// elementdan keyin yoki oldin kelgan elementni olish uchun
+
+
+// console.log(document.body.children);
+
+// for (node of document.body.childNodes) {
+//   if (node.nodeName === "#text") {
+//     continue;
+//   }
+//   console.log(node);
+// }
