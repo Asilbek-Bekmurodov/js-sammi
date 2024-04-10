1badd64d362a7a7d34dd04dddff9a07e638eee68
Asilbek2002 <school2924@gmail.com>
2024-04-11 00:39:51 +0500

    add loader

--- #34 loyiha/js/script.js
+++ #34 loyiha/js/script.js
@@ -2,7 +2,17 @@ window.addEventListener("DOMContentLoaded", () => {
   const tabsParent = document.querySelector(".tabheader__items"),
     tabs = document.querySelectorAll(".tabheader__item"),
     tabsContent = document.querySelectorAll(".tabcontent");
+  const loader = document.querySelector(".loader");
+  let num = 0;
+  // loader
+  setTimeout(() => {
+    loader.style.opacity = "0";
+    setTimeout(() => {
+      loader.style.display = "none";
+    }, 500);
+  }, 2000);
 
+  // tabs
   function hideTabContent() {
     tabsContent.forEach((item) => {
       item.classList.add("hide");
@@ -26,11 +36,23 @@ window.addEventListener("DOMContentLoaded", () => {
         if (event.target === item) {
           hideTabContent();
           showTabContent(idx);
+          num = idx;
         }
       });
     }
   });
 
   hideTabContent();
-  showTabContent(3);
+  showTabContent(num);
+
+  setInterval(() => {
+    if (num > 3) {
+      num = 0;
+    } else {
+      num++;
+    }
+    hideTabContent();
+    showTabContent(num);
+    console.log(num);
+  }, 3000);
 });
